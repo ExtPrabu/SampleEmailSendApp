c2ccae654a5658e116e99e66431eeb9285c61115
Prabuddha Das <prabuddha.das.ext@evidentscientific.com>
2024-12-27 20:10:23 +0530

    Add project files.

---
+++ SampleEmailSendApp/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace SampleEmailSendApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClickThis = new System.Windows.Forms.Button();
+            this.lblSendEmail = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnClickThis
+            // 
+            this.btnClickThis.Location = new System.Drawing.Point(305, 208);
+            this.btnClickThis.Name = "btnClickThis";
+            this.btnClickThis.Size = new System.Drawing.Size(171, 65);
+            this.btnClickThis.TabIndex = 0;
+            this.btnClickThis.Text = "Send Email";
+            this.btnClickThis.UseVisualStyleBackColor = true;
+            this.btnClickThis.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblSendEmail
+            // 
+            this.lblSendEmail.AutoSize = true;
+            this.lblSendEmail.Location = new System.Drawing.Point(362, 292);
+            this.lblSendEmail.Name = "lblSendEmail";
+            this.lblSendEmail.Size = new System.Drawing.Size(53, 13);
+            this.lblSendEmail.TabIndex = 1;
+            this.lblSendEmail.Text = "Click This";
+            // 
+            //// textBox1
+            //// 
+            //this.textBox1.Location = new System.Drawing.Point(516, 231);
+            //this.textBox1.Name = "textBox1";
+            //this.textBox1.Size = new System.Drawing.Size(100, 20);
+            //this.textBox1.TabIndex = 2;
+            //this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblSendEmail);
+            this.Controls.Add(this.btnClickThis);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickThis;
+        private System.Windows.Forms.Label lblSendEmail;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ SampleEmailSendApp/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Windows.Forms;
+
+namespace SampleEmailSendApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendSampleEmail();
+        }
+
+        private void SendSampleEmail()
+        {
+            try
+            {
+                
+                string senderEmail = "prabuddha.das.ext@evidentscientific.com"; 
+                string senderPassword = "Kitumiinfy@1234";       
+                string recipientEmail = "martin.fu.ext@evidentscientific.com"; 
+                string subject = "Test Email";
+                string body = "Hi Martin";
+
+                
+                SmtpClient smtpClient = new SmtpClient("smtp.gss.local", 587)
+                {
+                    Credentials = new NetworkCredential(senderEmail, senderPassword),
+                    EnableSsl = true 
+                };
+
+                
+                MailMessage mailMessage = new MailMessage
+                {
+                    From = new MailAddress(senderEmail),
+                    Subject = subject,
+                    Body = body,
+                    IsBodyHtml = false 
+                };
+
+                // Add the recipient
+                mailMessage.To.Add(recipientEmail);
+
+                // Send the email
+                smtpClient.Send(mailMessage);
+
+                MessageBox.Show("Sample email sent successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error sending email: {ex.Message}");
+            }
+        }
+    }
+}
